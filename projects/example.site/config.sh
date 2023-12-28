e6f75e8a987907e91c39d1fce9da66ae4cb79978
Kirsten Kluge <me@kirk.one>
2023-12-29 00:38:18 +0100

    Rename images

--- projects/example.site/config.sh
+++ projects/example.site/config.sh
@@ -2,6 +2,6 @@ site_title="example.site"
 theme="default"
 text_toggle=true
 social_button=true
-download_button=true
+download_button=false
 download_readme="All rights reserved Max Mustermann"
 backgroundcolor="#000000"
